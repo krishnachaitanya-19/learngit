0f2a59f2e2ed9a20382db183b8e5ea5230c857f1
krishna chaitanya <saichaitanyareddy19@gmail.com>
2021-12-29 15:29:03 +0530

    added multiply java to dev branch

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+import java.
+public class Multiply
+{
+public static void main(String[]args)
+{
+int a=10;
+int b=20;
+int multiply=a*b;
+System.out.println("product of 10 and 20 is"+multiply);
+}
+}
